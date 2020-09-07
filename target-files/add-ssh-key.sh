13592892c12b6bd208d011a91924b02cbaa39dfa
Craig Davis <kg5ohk@tutanota.com>
2020-09-07 16:48:03 -0500

    Test for directory then create

--- target-files/add-ssh-key.sh
+++ target-files/add-ssh-key.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 user=$1
-mkdir ~${user}/.ssh
+test -d ~${user}/.ssh || mkdir ~${user}/.ssh
 cat << 'EOS' > ~${user}/.ssh/authorized_keys
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcniUGavP7kbpertU/MKAcJL0O+PueCxIiCWomxZuUV stoney@platypus
 EOS
