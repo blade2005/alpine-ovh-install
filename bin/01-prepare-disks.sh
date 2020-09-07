086dd02aea8b2f802322cc1fd6c78eb424cb1ffd
Craig Davis <kg5ohk@tutanota.com>
2020-09-07 16:07:49 -0500

    shell isn't finding the function so skipping that

--- bin/01-prepare-disks.sh
+++ bin/01-prepare-disks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source ${SCRIPT_BASE}/install.env ${SCRIPT_BASE}/utils.sh
-notify "Prepare your disk. (partition, lvm, raid, mounting to ${chroot_dir}"
+echo "[-] Prepare your disk. (partition, lvm, raid, mounting to ${chroot_dir}"
 bash -l
--- bin/02-install-alpine-base.sh
+++ bin/02-install-alpine-base.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 source install.env utils.sh
-notify "Installing Alpine Linux with setup-alpine"
+echo "[-] Installing Alpine Linux with setup-alpine"
 
-notify "Preparing pristine alpine env"
+echo "[-] Preparing pristine alpine env"
 
-notify "Creating ${chroot_dir}"
+echo "[-] Creating ${chroot_dir}"
 test -d $chroot_dir || mkdir -p $chroot_dir
 
-notify "Downloading static apk tools"
+echo "[-] Downloading static apk tools"
 wget --no-clobber \
 	${mirror}/${branch}/main/${arch}/apk-tools-static-${version}.apk
 tar -xf apk-tools-static-${version}.apk
 
-notify "Installing alpine-base"
+echo "[-] Installing alpine-base"
 ./sbin/apk.static -X ${mirror}/${branch}/main -U --allow-untrusted \
 --root ${chroot_dir} --initdb add alpine-base
 
-notify "Setting up alpine-base"
+echo "[-] Setting up alpine-base"
 mkdir -p ${chroot_dir}/etc/apk
 
 cat repositories.template | envsubst > ${chroot_dir}/etc/apk/repositories
@@ -25,7 +25,7 @@ cat << RESOLV > ${chroot_dir}/etc/resolv.conf
 nameserver 1.1.1.1
 RESOLV
 
-notify "Mounting pseudo filesystems"
+echo "[-] Mounting pseudo filesystems"
 mount -t proc none ${chroot_dir}/proc
 mount -o bind /sys ${chroot_dir}/sys
 mount -o bind /dev ${chroot_dir}/dev
