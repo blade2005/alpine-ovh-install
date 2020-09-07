87eb61963b3d5ff02fa8f38d08d6c44b329d80c8
Craig Davis <kg5ohk@tutanota.com>
2020-09-07 16:20:32 -0500

    breakfix add-ssh-key installing to wrong user

--- target-files/add-ssh-key.sh
+++ target-files/add-ssh-key.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 user=$1
 mkdir ~${user}/.ssh
-cat << 'EOS' > ~/.ssh/authorized_keys
+cat << 'EOS' > ~${user}/.ssh/authorized_keys
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcniUGavP7kbpertU/MKAcJL0O+PueCxIiCWomxZuUV stoney@platypus
 EOS
 
