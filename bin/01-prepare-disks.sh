ffa7ee958a721c3f88ac0f80d2f87cc6e5c569f2
Craig Davis <kg5ohk@tutanota.com>
2020-09-07 16:03:11 -0500

    Fix line split issue

--- bin/01-prepare-disks.sh
+++ bin/01-prepare-disks.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 source install.env utils.sh
-notify "Prepare your disk. (partition, lvm, raid, mounting to
-${chroot_dir}"
-sh -l
+notify "Prepare your disk. (partition, lvm, raid, mounting to ${chroot_dir}"
+bash -l
